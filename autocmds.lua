949a32bd6c6d19f5ba083c0d5e36199415a0ec0a
sc-zhang <zsc-zhang@foxmail.com>
2025-12-25 09:18:40 +0800

    fix bug that when row=1, could not restore pos

--- autocmds.lua
+++ autocmds.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
-    if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+    if mark[1] >= 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
       vim.api.nvim_win_set_cursor(0, { mark[1], mark[2] })
     end
   end,
